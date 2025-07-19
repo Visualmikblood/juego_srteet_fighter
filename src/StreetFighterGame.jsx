91c06ec55cf927e4c433438d5e2e40eb114e8cf3
Miguel <1bcmigueliglesias@gmail.com>
2025-07-19 20:30:50 +0200

    he puesto la url que me dio railway

--- src/StreetFighterGame.jsx
+++ src/StreetFighterGame.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect, useCallback, useRef } from 'react';
 import { io } from 'socket.io-client';
 
-const socket = io('http://localhost:3001');
+const socket = io("https://backendjuegostreetfighter-production.up.railway.app");
 
 const StreetFighterGame = () => {
   const [gameState, setGameState] = useState({
